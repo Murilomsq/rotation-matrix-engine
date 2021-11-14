5933d47c594b74881f271bd463506289b468a001
Murilomsq <56661791+Murilomsq@users.noreply.github.com>
2021-11-14 19:24:30 -0300

    Update rot.m

--- rot.m
+++ rot.m
@@ -1,4 +1,4 @@
-function [x,y] = fasor(matrix, ang)
+function [x,y] = rotate(matrix, ang)
 deg = deg2rad(ang);
 RotMatrix = [cos(deg) -sin(deg); sin(deg) cos(deg)];
 Product = RotMatrix * [matrix(1); matrix(2)]
